d50c9259784124dbb7ecaa6e64f062bf7519820d
JUDITH Florian <florian.judith@economat-armees.fr>
2017-08-10 17:39:21 +0200

    Updated md-preview to 1.3.0

--- assets/install_md-preview.sh
+++ assets/install_md-preview.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 pushd /alfresco/amps
-wget https://github.com/fjudith/md-preview/releases/download/1.2.1/parashift-mdpreview-repo-1.2.1.amp
+wget https://github.com/fjudith/md-preview/releases/download/1.3.0/parashift-mdpreview-repo-1.3.0.amp
 pushd /alfresco/amps_share
-wget https://github.com/fjudith/md-preview/releases/download/1.2.1/parashift-mdpreview-share-1.2.1.amp
+wget https://github.com/fjudith/md-preview/releases/download/1.3.0/parashift-mdpreview-share-1.3.0.amp
 /alfresco/bin/apply_amps.sh
 popd
