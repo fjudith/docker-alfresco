1c380a8ae1aae1aaab0b65a674fac13aa12045ea
JUDITH Florian <florian.judith@economat-armees.fr>
2017-11-19 02:26:31 +0100

    Implement reverse-proxy

--- share/docker-entrypoint.sh
+++ share/docker-entrypoint.sh
@@ -23,21 +23,45 @@ function set_reverse_proxy {
     echo "INFO: Reverse proxy not configured"
   else
     echo "INFO: Configuring alfresco for independant reverse-proxy support"
-
+    
+    SHARE_SECURITY_CONFIG="${CATALINA_HOME}/webapps/share/WEB-INF/classes/alfresco/share-security-config.xml"
+    SHARE_SECURITY_TEMP="${CATALINA_HOME}/webapps/share/WEB-INF/classes/alfresco/share-security-config.xml.tmp"
+    SHARE_CONFIG_CUSTOM="${CATALINA_HOME}/shared/classes/alfresco/web-extension/share-config-custom.xml"
+    
+    # Write CSRF node in temp file
+    xmlstarlet sel -E utf-8 -t -c '/alfresco-config/config[@condition="CSRFPolicy" and not(@replace)]' ${SHARE_SECURITY_CONFIG} > ${SHARE_SECURITY_TEMP}
+     
+    # Insert rever-proxy config in temp file
     xmlstarlet ed  \
-        -P -S -L \
-        -i '/alfresco-config/config[@condition="CSRFPolicy" and not(@replace)]' \
+        -L \
+        -i '/config[@condition="CSRFPolicy" and not(@replace)]' \
         -t 'attr' -n 'replace' -v 'true' \
-        -s '/alfresco-config/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertOrigin"]' \
+        -s '/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertOrigin"]' \
         -t 'elem' -n 'param' -v "$REVERSE_PROXY_URL" \
-        -i '/alfresco-config/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertOrigin"]/param[not(@name)]' \
+        -i '/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertOrigin"]/param[not(@name)]' \
         -t 'attr' -n 'name' -v 'origin' \
-        -s '/alfresco-config/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertReferer"]' \
+        -s '/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertReferer"]' \
         -t 'elem' -n 'param' -v "$REVERSE_PROXY_URL/.*" \
-        -i '/alfresco-config/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertReferer"]/param[not(@name)]' \
+        -i '/config[@condition="CSRFPolicy"]/filter/rule/action[@name="assertReferer"]/param[not(@name)]' \
         -t 'attr' -n 'name' -v 'referer' \
-        $CATALINA_HOME/shared/classes/alfresco/web-extension/share-config-custom.xml
-  
+        ${SHARE_SECURITY_TEMP}
+
+    # Backup Restore share-config-custom.xml to prevent doubled insertion
+    if ! [ -f ${SHARE_CONFIG_CUSTOM}.backup ]; then
+      cp ${SHARE_CONFIG_CUSTOM} ${SHARE_CONFIG_CUSTOM}.backup
+    else
+      cp ${SHARE_CONFIG_CUSTOM}.backup ${SHARE_CONFIG_CUSTOM}
+    fi
+
+    # Remove closing root node
+    sed -i 's/<\/alfresco\-config>//g' ${SHARE_CONFIG_CUSTOM}
+    # Insert CSRF config in share-config-custom.xml
+    xmlstarlet sel -E utf-8 -t -c '/config[@condition="CSRFPolicy" and (@replace)="true"]' ${SHARE_SECURITY_TEMP} >> ${SHARE_CONFIG_CUSTOM}
+    # Restore closing root node
+    echo '</alfresco-config>' >> ${SHARE_CONFIG_CUSTOM}
+
+    # Remove temp file
+    rm -f ${SHARE_SECURITY_TEMP}
   fi
 
   echo ------------------------------
